425c3188764a0fd70ba4bad11da04a5f20bac1c0
kirankalsi <kiran.1998@hotmail.co.uk>
2020-11-09 15:49:14 +0000

    structure created

---
+++ application/models.py
@@ -0,0 +1,14 @@
+from application import db
+
+class Genres(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    genre_type = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    rating = db.Column(db.Float), nullable=False)
+    films = db.relationship('Films', backref='genre')
+
+class Films(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    duration = db.Column(db.String(20), nullable=False)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'), nullable=False)
+    age_rating = db.Column(db.String(5), nullable=False)
