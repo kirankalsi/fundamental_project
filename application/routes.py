3c48733e097fd40a5828fb3a1d9ebd1014fb774b
kirankalsi <kiran.1998@hotmail.co.uk>
2020-11-11 20:49:09 +0000

    add genre and film route separate

--- application/routes.py
+++ application/routes.py
@@ -2,19 +2,30 @@ from flask import render_template, redirect, url_for, request
 
 from application import app, db
 from application.models import Films, Genres, FilmGenres
-from application.forms import FilmForm, SameFilmCheck
+from application.forms import GenreForm, FilmForm, SameFilmCheck
 
 @app.route('/')
 def index():
+    genre_list = Genres.query.all()
     film_list = Films.query.all()
-    return render_template('index.html')
+    return render_template('index.html', genre_list=genre_list, film_list=film_list)
 
-@app.route('/add', methods=['GET','POST'])
-def add():
+@app.route('/add_genre', methods=['GET','POST'])
+def add_genre():
+    form = GenreForm()
+    if form.validate_on_submit():
+        new_genre = Genre(genre_type=form.genre.data)
+        db.session.add(new_genre)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_genre.html', form=form)
+
+@app.route('/add_film', methods=['GET','POST'])
+def add_film():
     form = FilmForm()
     if form.validate_on_submit():
         new_film = Films(title=form.film.data)
         db.session.add(new_film)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('add.html', form=form)
+    return render_template('add_film.html', form=form)
