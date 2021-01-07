8f595bb64c6d4355d2fe060ce4112847600f98e8
Ubuntu <azureuser@vm.cpser2dc0oje3as2ejlu5im3sc.zx.internal.cloudapp.net>
2021-01-07 16:40:27 +0000

    kubernetes

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DB_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
